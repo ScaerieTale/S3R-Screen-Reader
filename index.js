974fb4c5a1b05060b5400f9e8f2fdf7c710806f4
ScaerieTale <scaerietale@gmail.com>
2024-11-06 15:24:53 -0600

    temporarily connect 'Speak!' button to console.log

---
+++ index.js
@@ -0,0 +1,6 @@
+function Speak() {
+    // Get text from textInput and
+    //log it to the console for testing
+    let input = document.getElementById("textInput").value;
+    console.log(input);
+}
