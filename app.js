b97aff23efea59507b5fa88eef647f9307d7cccd
ScaerieTale <scaerietale@gmail.com>
2024-11-08 09:57:08 -0600

    Clear all files to start refactoring from ground up

--- app.js
+++
@@ -1,8 +0,0 @@
-function Speak() {
-    const input = new SpeechSynthesisUtterance(document.getElementById("textInput").value);
-    speechSynthesis.speak(input);
-}
-/* TODO:
-    Add voice options
-    Add voice speed
-    Add voice pitch */
