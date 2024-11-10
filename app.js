f5e0517bbe81646b00d33007a73e0861f346e02f
ScaerieTale <scaerietale@gmail.com>
2024-11-09 22:33:49 -0600

    add missing const #voices

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const synth = window.SpeechSynthesis;
 const text = document.getElementById("textInput");
 const speed = document.getElementById("rate");
 const pitch = document.getElementById("pitch");
+const voices = document.getElementById("voices");
+
+
 
 function OnClick() {
     console.log(text.value);
