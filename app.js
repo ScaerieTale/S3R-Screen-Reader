88424a10eb0f9e8ad630b9a46a1ce2df3da965d7
ScaerieTale <scaerietale@gmail.com>
2024-11-09 23:43:16 -0600

    Set up voice list to be populated Fix for the speechSynthesis API not working at all

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
-// constants
-const synth = window.SpeechSynthesis;
-// grabbing elements from the DOM
+// Initialize voice API
+const synth = window.speechSynthesis;
+
+// DOM values as vars
 const inputForm = document.querySelector("form");
 const text = document.getElementById("textInput");
 const speechRate = document.getElementById("rate");
 const rateValue = document.getElementById("rate-value");
 const pitch = document.getElementById("pitch");
 const pitchValue = document.getElementById("pitch-value");
-const voices = document.getElementById("voices");
+const voiceSelect = document.getElementById("voices");
+
+// voice select setup
+let voices = [];
+function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (const voice of voices) {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name} (${voice.lang})`;
+  
+      if (voice.default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voice.lang);
+      option.setAttribute("data-name", voice.name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
 
 
 
+/* let voiceArray = [];
+
+const getVoices = () => {
+    voiceArray = synth.getVoices();
+    voiceArray.forEach(voice => {
+        const option = CSSLayerStatement();
+        option.textContent = voice.name + '(' + voice.lang + ')';
+    })
+    
+}
+
+getVoices();
+if (synth.onvoiceschanged != undefined) {
+    synth.onvoiceschanged = getVoices();
+}
+*/
