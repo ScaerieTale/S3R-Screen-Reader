e5aff0492933e4d7c88ff83dd20f1d8940f5c083
ScaerieTale <scaerietale@gmail.com>
2024-11-15 20:58:40 -0600

    Fix for voice selector dropdown not working getAttribute() not defined error

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-// Initialize voice API
+// import web speech synthesis API as synth
 const synth = window.speechSynthesis;
 
-// DOM values as vars
+// get DOM elements
 const inputForm = document.querySelector("form");
-const text = document.getElementById("textInput");
-const speechRate = document.getElementById("rate");
-const rateValue = document.getElementById("rate-value");
+const inputTxt = document.querySelector("textarea");
+const voiceSelect = document.querySelector("select");
 const pitch = document.getElementById("pitch");
 const pitchValue = document.getElementById("pitch-value");
-const voiceSelect = document.getElementById("voices");
+const rate = document.getElementById("rate");
+const rateValue = document.getElementById("rate-value");
 
-// voice select setup
+// set up voice selection
 let voices = [];
-function populateVoiceList() {
-    voices = synth.getVoices();
-  
-    for (const voice of voices) {
-      const option = document.createElement("option");
-      option.textContent = `${voice.name} (${voice.lang})`;
-  
-      if (voice.default) {
-        option.textContent += " — DEFAULT";
-      }
-  
-      option.setAttribute("data-lang", voice.lang);
-      option.setAttribute("data-name", voice.name);
-      voiceSelect.appendChild(option);
+
+function setVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    // Mark the default voice
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    // set voice details in list
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+setVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = setVoiceList;
+}
+
+inputForm.onsubmit = (event) => {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
     }
   }
-  
-  populateVoiceList();
-  if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
-  }
+  // set pitch and speed
+  utterThis.pitch = pitch.value;
+  utterThis.rate = rate.value;
+  synth.speak(utterThis);
 
-// Speech action
-
-function Speak() {
-    // Check if text is being spoken currently.
-    if (synth.speaking) {
-        synth.pause();
-        return;
-    }
-    else if (synth.paused) {
-        synth.resume();
-        return;
-    }
-    // voice selection
-    
-    
-    // Only run if text input box is NOT empty.
-    if (text.value != '') {
-        let speakText = new SpeechSynthesisUtterance(text.value);
-        synth.speak(speakText);
-    } 
+  inputTxt.blur();
 }
